4f2f594adb4c8747cdac5f6560cbac120bc61357
AlexValder <anna.loban@yahoo.com>
2022-01-22 18:09:16 +0200

    Resolution settings

--- Scenes/SceneScripts/left_panel.gd
+++ Scenes/SceneScripts/left_panel.gd
@@ -2,6 +2,8 @@ extends VBoxContainer
 
 onready var _panel := $panel as Control
 onready var _hide_button := $hbox/HideShowGui as Button
+onready var _res_option := \
+    $panel/Settings/ResolutionOptionButton as OptionButton
 
 var _panel_hidden := false
 
@@ -29,3 +31,14 @@ func _on_FullscreenCheckButton_toggled(button_pressed: bool) -> void:
 
 func _on_ExitButton_button_up() -> void:
     get_tree().quit(0)
+
+
+func _on_ResolutionOptionButton_item_selected(index: int) -> void:
+    var text := _res_option.get_item_text(index)
+    var size : Vector2
+    if text.count("Native") == 0:
+        var values := text.split("x", false, 2)
+        size = Vector2(values[0].to_float(), values[1].to_float())
+    else:
+        size = OS.get_real_window_size()
+    get_viewport().size = size
